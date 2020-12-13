a9394d714785b36f1096b3f9b3c6756a6fa8055f
MiniMamini01 <74473038+MiniMamini01@users.noreply.github.com>
2020-12-13 22:19:17 +0330

    Add files via upload

---
+++ simple_virus.py
@@ -0,0 +1,21 @@
+import os 
+
+def search(path):
+    my_final_list = []
+    files = os.listdir(r'C:\Users\minim\Desktop\test1')
+    for element in files:
+        if os.path.isdir(path + "\\" + element) == True:
+            open(path+ '\\' + element + '\\'+ 'i am virus.txt' , 'x')
+            wf = open(path + '\\' + element + '\\' + 'i am virus.txt' , 'w')
+            wf.write('< i am a simple virus , hello > ')
+            wf.close()
+        elif element[-4:] == '.txt':
+            rf = open(path + '\\' + element , 'r')
+            tmp = rf.read()
+            af = open(path + '\\' + element , 'w')
+            af.write('<i am a simple virus , hello >' + tmp )
+        else:
+            pass  
+        
+search(r'C:\Users\minim\Desktop\test1')
+
